b39d6e826c49960075eec34da61bb3aedae391bc
tokytank <91239056+tokytank@users.noreply.github.com>
2023-09-28 20:04:31 +0800

    init

---
+++ GItTest/toky.cpp
@@ -0,0 +1,11 @@
+#include "toky.h"
+#include <vector>
+void Toky::Update(float DeltaTime)
+{
+	vector<int>onlineArr={1,2,3};
+}
+
+bool Toky::BeginPlay()
+{
+	return false;
+}
---
+++ GItTest/toky.h
@@ -0,0 +1,7 @@
+class Toky
+{
+public:
+	virtual ~Toky() = default;
+	virtual void Update(float DeltaTime);
+	virtual bool BeginPlay();
+};
