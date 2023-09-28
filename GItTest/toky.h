ee0321d0e97c7e2c212d4150a56051ae740c3390
tokytank <91239056+tokytank@users.noreply.github.com>
2023-09-28 20:38:56 +0800

    reset ones

--- GItTest/toky.h
+++ GItTest/toky.h
@@ -2,3 +2,35 @@ void fun()
 {
 	cout<<1;
 }
+
+void fun1()
+{
+
+}
+void fun()
+{
+	cout<<1;
+}
+
+void fun1()
+{
+	
+}
+void fun2()
+{
+	cout<<1;
+}
+
+void fun1()
+{
+	
+}
+void fun()
+{
+	cout<<1;
+}
+
+void fun1()
+{
+	
+}
